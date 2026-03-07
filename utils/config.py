2fab8415d859ab8b1d2b077bdc31795db17a8af4
Doktorzivota <luka.milutinovic01@gmail.com>
2026-03-07 19:55:13 +0100

    Add initial KVAE model skeleton

--- utils/config.py
+++ utils/config.py
@@ -24,3 +24,22 @@ class SimulationConfig:
     # physics 
     gravity_strength_scale: float = 0.002  # gravity strength proportion
 
+from dataclasses import dataclass
+
+@dataclass
+class VAEConfig:
+    # Kalman filter dimensions 
+    dim_a: int = 2                         # dimension of action vector
+    dim_z: int = 4                         # dimension of latent state
+    dim_u: int = 0                         # dimension of control input (set to 0 if not used)
+    num_matrices: int = 3                  # number of different matrices to experiment with
+
+    # standard deviations for initialization 
+    B_std: float = 0.01                    # standard deviation for B matrices
+    C_std: float = 0.01                    # standard deviation for C matrices
+    Q_std: float = 0.025                   # standard deviation for process noise covariance Q
+    R_std: float = 0.01                    # standard deviation for observation noise covariance R
+
+
+
+    
